3d20e26f4087e815e7ba4fd8d866d87ef4fa0c55
liu_yalong <4379711@qq.com>
2019-03-27 14:23:06 +0800

    修改代码结构，将规则单独列出

--- amz.py
+++ amz.py
@@ -1,15 +1,25 @@
-import requests
 import re
+import requests
 from urllib import parse
 import lxml.etree
 from tools_mysql import *
 from tools import *
 from mylog import My_log
+from spider_rules import *
+
+lst = Listing_Rules()
+sp_key = SP_by_key()
+rule_by_key = Search_by_key()
 
 logger = My_log('lyl').get_logger()
 
 
 def parse_robot(html):
+    """
+    如果被发现是机器，人工输入验证码进行验证
+    :param html: 验证码页面html
+    :return: 返回正常请求的页面html str
+    """
     mytree = lxml.etree.HTML(html)
     amzn = mytree.xpath('//form[@action="/errors/validateCaptcha"]/input[@name="amzn"]/@value')
     amzn_r = mytree.xpath('//form[@action="/errors/validateCaptcha"]/input[@name="amzn-r"]/@value')
@@ -70,14 +80,6 @@ def get_sell_time(asin, page):
         return 'null'
 
 
-def rules_title():
-    rules = [('re_S', '<span id="productTitle" class="a-size-large">(.*?)</span>?'),
-             ('xpath', '//span[@id="btAsinTitle"]/text()'),
-             ('xpath', '//h1[@data-automation-id="title"]/text()'),
-             ]
-    return rules
-
-
 def listing_uk(asin):
     """
     通过asin获取商品的详细信息
@@ -106,79 +108,97 @@ def listing_uk(asin):
 
     if is_robot(a):
         a = parse_robot(a)
-        # return listing
 
     mytree = lxml.etree.HTML(a)
+
     # 排名情况
-    rank_list1 = mytree.xpath('//li[@id="SalesRank"]/text()')
-    rank_list2 = mytree.xpath('//tr[@id="SalesRank"]/td[@class="value"]/text()')
-    rank1 = list_to_str(rank_list1)
-    rank2 = list_to_str(rank_list2)
-    if len(rank1) > 0:
-        ranks = re.findall('(.*) in (.*)\(', rank1)
-        # 大类中排名
-        listing['大类中排名'] = ranks[0][0].replace(' ', '')
-        # 大类名称
-        listing['大类名字'] = ranks[0][1]
-    elif len(rank2) > 0:
-        ranks = re.findall('(.*) in (.*)\(', rank2)
-        # 大类中排名
-        listing['大类中排名'] = ranks[0][0].replace(' ', '')
-        # 大类名称
-        listing['大类名字'] = ranks[0][1]
+    for rank_rule in lst.rules_rank():
+        if rank_rule[0] == 're_S':
+            ranks = re.findall(rank_rule[1], a, re.S)
+            ranks = re_clear_str(ranks)
+            if len(ranks) > 0:
+                ranks = re.findall('(.*) in (.*)\(', ranks)
+                # 大类中排名
+                listing['大类中排名'] = ranks[0][0].replace(' ', '')
+                # 大类名称
+                listing['大类名字'] = ranks[0][1]
+                break
+        elif rank_rule[0] == 'xpath':
+            ranks = mytree.xpath(rank_rule[1])
+            ranks = re_clear_str(ranks)
+            if len(ranks) > 0:
+                ranks = re.findall('(.*) in (.*)\(', ranks)
+                # 大类中排名
+                listing['大类中排名'] = ranks[0][0].replace(' ', '')
+                # 大类名称
+                listing['大类名字'] = ranks[0][1]
+                break
+        else:
+            ranks = re.findall(rank_rule[1], a)
+            ranks = re_clear_str(ranks)
+            if len(ranks) > 0:
+                ranks = re.findall('(.*) in (.*)\(', ranks)
+                # 大类中排名
+                listing['大类中排名'] = ranks[0][0].replace(' ', '')
+                # 大类名称
+                listing['大类名字'] = ranks[0][1]
+                break
 
     # 子类排名情况
-    child_rank_list1 = mytree.xpath('//*[@id="SalesRank"]//ul')
-    if len(child_rank_list1) > 0:
-        child_rank_list1 = child_rank_list1[0].xpath('string(.)')
-        child_rank1 = list_to_str(child_rank_list1)
-        listing['子类排名'] = child_rank1
+    for child_rank in lst.rules_child_rank():
+        if child_rank[0] == 'xpath':
+            ranks = mytree.xpath(child_rank[1])
+            if len(ranks) > 0:
+                child_rank_list1 = ranks[0].xpath('string(.)')
+                child_rank1 = list_to_str(child_rank_list1)
+                listing['子类排名'] = child_rank1
+                break
 
     # 评论个数
-    rev1 = mytree.xpath('//span[@id="acrCustomerReviewText"]//text()')
-    rev2 = re.findall('">([\d, ]+)customer reviews', a)
-    if len(rev1) > 0:
-        rev1_tmp = rev1[0].replace('customer reviews', '').replace('customer review', '')
-        listing['评论数'] = rev1_tmp
-        # print('评论1')
-    elif len(rev2) > 0:
-        # print('评论2')
-        listing['评论数'] = rev2[0]
+    for rev in lst.rules_rev():
+        if rev[0] == 'xpath':
+            revs = mytree.xpath(rev[1])
+            if len(revs) > 0:
+                rev1_tmp = revs[0].replace('customer reviews', '').replace('customer review', '')
+                listing['评论数'] = rev1_tmp
+                break
+        elif rev[0] == 're':
+            revs = re.findall(rev[1], a)
+            if len(revs) > 0:
+                listing['评论数'] = revs[0]
+                break
 
     # 评分，星级
-    score = re.findall('class="reviewCountTextLinkedHistogram noUnderline" title="(.*) out of 5 stars', a)
-    score2 = re.findall('>([\d. ]+)out of 5 stars', a)
-    if len(score) > 0:
-        listing['星级'] = score[0]
-    elif len(score2) > 0:
-        listing['星级'] = score2[0]
+    for score in lst.rules_scores():
+        if score[0] == 're':
+            tmp = re.findall(score[1], a)
+            if len(tmp) > 0:
+                listing['星级'] = tmp[0]
+                break
 
     # 价格
-    price1 = re.findall('class="a-size-medium a-color-price">(.*)</span>', a)
-    price2 = re.findall('class="a-color-price a-text-bold">(.*)</span>', a)
-    price3 = mytree.xpath('//td[@class="a-color-price a-size-medium a-align-bottom"]/text()')
-    price4 = mytree.xpath('//button[@class="av-button av-button--default js-purchase-button dv-record-reftag"]/text()')
+    for price in lst.rules_price():
+        if price[0] == 're':
+            price1 = re.findall(price[1], a)
+            if len(price1) > 0:
+                listing['价格'] = price1[0]
+                break
+        elif price[0] == 'xpath':
+            price3 = mytree.xpath(price[1])
+            if len(price3) > 0:
+                listing['价格'] = price3[0]
+                break
 
-    if len(price1) > 0:
-        listing['价格'] = price1[0]
-    elif len(price2) > 0:
-        listing['价格'] = price2[0]
-    elif len(price3) > 0:
-        listing['价格'] = price3[0]
-    elif len(price4) > 0:
-        listing['价格'] = price4[3]
-
-    # 品牌01
-    pinpai1 = re.findall('id="bylineInfo".*>+(.*)</a>', a)
-    pinpai2 = re.findall('id="gc-brand-name-link".*>+(.*)</a>', a)
-
-    if len(pinpai1) > 0:
-        listing['品牌'] = pinpai1[0]
-    elif len(pinpai2) > 0:
-        listing['品牌'] = pinpai2[0]
+    # 品牌
+    for pp in lst.rules_pinpai():
+        if pp[0] == 're':
+            pinpai = re.findall(pp[1], a)
+            if len(pinpai) > 0:
+                listing['品牌'] = pinpai[0]
+                break
 
     # 标题
-    for title_rule in rules_title():
+    for title_rule in lst.rules_title():
         if title_rule[0] == 're_S':
             titles = re.findall(title_rule[1], a, re.S)
             titles = re_clear_str(titles)
@@ -196,30 +216,19 @@ def listing_uk(asin):
             if len(titles) > 0:
                 listing['标题'] = titles
                 break
-    # titles1 = re.findall('<span id="productTitle" class="a-size-large">(.*?)</span>?', a, re.S)
-    # titles1 = re_clear_str(titles1)
-    # titles2 = mytree.xpath('//span[@id="btAsinTitle"]/text()')
-    # titles3 = mytree.xpath('//h1[@data-automation-id="title"]/text()')
-    #
-    # if len(titles1) > 0:
-    #     listing['标题'] = titles1
-    # elif len(titles2) > 0:
-    #     listing['标题'] = titles2[0]
-    # elif len(titles3) > 0:
-    #     listing['标题'] = titles3[0]
 
     # 上架时间（如果能找到发布的上架时间信息，如果没有，取评论时间-15天）
-    # sell_time1 = re.findall('<td class="label">Date First Available</td>', a)
-    sell_time2 = mytree.xpath('//tr[@class="date-first-available"]/td[@class="value"]/text()')
-    sell_time3 = re.findall('<li><b> Date first available at Amazon.co.uk:</b> (.*)</li>', a)
-
-    # if len(sell_time1) > 0:
-    #     listing['上架时间'] = sell_time1[0]
-    if len(sell_time2) > 0:
-        listing['上架时间'] = sell_time2[0]
-    elif len(sell_time3) > 0:
-        listing['上架时间'] = sell_time3[0]
-
+    for sell in lst.rules_sell_time():
+        if sell[0] == 'xpath':
+            sell_time = mytree.xpath(sell[1])
+            if len(sell_time) > 0:
+                listing['上架时间'] = sell_time[0]
+                break
+        elif sell[0] == 're':
+            sell_time = re.findall(sell[1], a)
+            if len(sell_time) > 0:
+                listing['上架时间'] = sell_time[0]
+                break
     # 如果商品页没有上架时间，取评论时间
     if listing['上架时间'] == 'null':
         if listing['评论数'] != 'null':
@@ -232,30 +241,26 @@ def listing_uk(asin):
                 listing['上架时间'] = get_sell_time(asin, page)
             except Exception as e:
                 logger.error('asin:{}评论数转为页码失败,{}'.format(asin, e))
-                # print('评论数转为页码失败', e)
 
     # 图片url
-    pic_list = mytree.xpath('//div[@class="imgTagWrapper"]/img/@src')
-    pic_list2 = mytree.xpath('//div[@class="av-fallback-packshot"]/img/@src')
-    pic_list00 = mytree.xpath('//div[@class="imgTagWrapper"]/img/@data-old-hires')
-    pic_list01 = mytree.xpath('//div[@class="imgTagWrapper"]/img/@data-a-dynamic-image')
+    for pic in lst.rules_pic():
+        if pic[0] == 'xpath':
+            pic_list = mytree.xpath(pic[1])
+            if len(pic_list) > 0 and len(pic_list[0]) < 100 and len(pic_list[0]) > 0:
+                listing['图片链接'] = pic_list[0]
+                break
+            elif len(pic_list) > 0 and len(pic_list) > 0:
+                pics = re.findall('https://.*?.jpg', pic_list[0])
+                if len(pics) > 0:
+                    listing['图片链接'] = pics[0]
 
-    if len(pic_list) > 0 and len(pic_list[0]) < 100:
-        listing['图片链接'] = pic_list[0]
-    elif len(pic_list2) > 0 and len(pic_list2[0]) < 100:
-        listing['图片链接'] = pic_list2[0]
-    else:
-        if len(pic_list00) > 0 and len(pic_list00[0]) > 0:
-            listing['图片链接'] = pic_list00[0]
-        elif len(pic_list01) > 0 and len(pic_list01[0]) > 0:
-            l = re.findall('https://.*?.jpg', pic_list01[0])
-            if len(l) > 0:
-                listing['图片链接'] = l[0]
+    # 自营
+    for ziying in lst.rules_ziying():
+        if ziying[0] == 're':
+            is_ziying = re.findall(ziying[1], a)
+            if len(is_ziying) > 0:
+                listing['是否自营'] = 'yes'
 
-    # 自营(如果为空，表示非自营，如果有，有可能是自营)
-    is_ziying = re.findall('sold by Amazon.', a)
-    if len(is_ziying) > 0:
-        listing['是否自营'] = 'yes'
     print(listing.items())
 
 
@@ -386,70 +391,47 @@ def asins_by_key(key, page):
     查询第page页的商品asin
     :param key: 关键字，以空格区别多个关键字
     :param page: 页码
-    :return: 返回asin(去掉广告)
+    :return: 返回asin列表(去掉广告后)
     """
-    # TODO 根据page请求数量，调用search_by_key函数
     html = search_by_key(key, page)
     if is_robot(html):
-        return []
+        html = parse_robot(html)
 
     mytree = lxml.etree.HTML(html)
-    # 通过xpath格式化，转为字符串，以供正则使用
+    # 通过xpath格式化，转为字符串
     html_reg = lxml.etree.tostring(mytree, encoding=str, pretty_print=True)
-    sp_asin = find_sp_asins(html_reg)
-    if len(sp_asin) > 0:
-        print('广告1', sp_asin)
-    else:
-        sp_asin = find_sp_asins2(mytree)
-        print('广告2', sp_asin)
-    # 各种规则
-    asin1 = mytree.xpath('//div[contains(@data-cel-widget,"search_result_")]/@data-asin')
+    # 正则去掉注释部分
+    html_reg = re.sub('<!--[\s\S]*?-->', '', html_reg)
+    sp_asin = []
+    asin_result = []
+    # 找出广告的asin列表
+    for sps in sp_key.sp():
+        if sps[0] == 're_S':
+            sp_asin = re.findall(sps[1], html_reg, re.S)
 
-    if len(asin1) > 0:
-        asin = asin1
-        print('解析规则：1', len(asin))
-    else:
-        asin2 = mytree.xpath('//div[@class="s-result-list sg-row"]//div/@data-asin')
-        if len(asin2) > 0:
-            asin = asin2
-            print('解析规则：2', len(asin))
+        elif sps[0] == 're':
+            sp_asin = re.findall(sps[1], html_reg)
+
+        elif sps[0] == 'xpath_replace':
+            sps = mytree.xpath(sps[1])
+            sp_asin = [k.replace('a-popover-sp-info-popover-', '') for k in sps]
         else:
-            asin3 = mytree.xpath('//li[contains(@id,"result_")]//@data-asin')
-            asin = asin3
-            print('解析规则：3', len(asin))
-    result_asin = clear_other_list(asin, sp_asin)
+            sp_asin = []
+        if len(sp_asin) > 0:
+            break
+
+    # 各种规则
+    for rules in rule_by_key.asin_by_key():
+        if rules[0] == 'xpath':
+            asin = mytree.xpath(rules[1])
+            if len(asin) > 0:
+                asin_result = asin
+    result_asin = clear_other_list(asin_result, sp_asin)
+    print(f'总asin{len(asin_result)}个：', asin_result)
+    print(f'广告asin共{len(sp_asin)}个：：', sp_asin)
+    print(f'清洗后asin{len(result_asin)}个：:', result_asin)
+
     return result_asin
-
-
-def find_sp_asins(html):
-    # TODO  <!--[\s\S]*?--> 注释内容会被找到,应需要剔除，但不影响结果
-    html = re.sub('<!--[\s\S]*?-->', '', html)
-
-    # 找出广告的asin
-    # # ads = re.findall('These are ads', html)
-    # if len(ads) < 1:
-    #     return []
-    # else:
-    #     print('预计广告数量：', len(ads))
-    regex_asin_list = re.compile('<div.*?="a-popover-sponsored-header-(.*?)"><span>These are ads')
-    aa = regex_asin_list.findall(html)
-
-    if len(aa) > 0:
-        return aa
-    else:
-        bb = re.findall('<div.*?="a-popover-sp-info-popover-(.*?)"><span>These are ads', html, re.S)
-        return bb
-
-
-def find_sp_asins2(mytree):
-    sps = mytree.xpath("//div[contains(@id,'a-popover-sp-info-popover-')]/@id")
-    sp = [k.replace('a-popover-sp-info-popover-', '') for k in sps]
-
-    if len(sp) > 0:
-        return sp
-    else:
-        print('find_sp_asins2未匹配到广告')
-        return []
 
 
 if __name__ == '__main__':
@@ -463,9 +445,8 @@ if __name__ == '__main__':
     # print(len(asins))
 
     # print(asins)
-    listing_uk('B01DZ5HR66')
+    # listing_uk('B000WI02HG')
     # get_sell_time('B01E8ZKD3G', 2)
 
-    # asins = asins_by_key('bluetooth headphones', 1)
-    # print(asins)
-    # print('原', len(asins), '去重', len(set(asins)))
+    asins_by_key('bluetooth headphones', 1)
+
---
+++ spider_rules.py
@@ -0,0 +1,110 @@
+class Listing_Rules:
+    """亚马逊商品详情页爬虫规则"""
+
+    # 标题
+    @staticmethod
+    def rules_title():
+        rules = [('re_S', '<span id="productTitle" class="a-size-large">(.*?)</span>?'),
+                 ('xpath', '//span[@id="btAsinTitle"]/text()'),
+                 ('xpath', '//h1[@data-automation-id="title"]/text()'),
+                 ]
+        return rules
+
+    # 大类排名
+    @staticmethod
+    def rules_rank():
+        rules = [
+            ('xpath', '//li[@id="SalesRank"]/text()'),
+            ('xpath', '//tr[@id="SalesRank"]/td[@class="value"]/text()'),
+        ]
+        return rules
+
+    # 子类排名
+    @staticmethod
+    def rules_child_rank():
+        rules = [
+            ('xpath', '//*[@id="SalesRank"]//ul'),
+        ]
+        return rules
+
+    # 评论数量
+    @staticmethod
+    def rules_rev():
+        rules = [('re', '">([\d, ]+)customer reviews'),
+                 ('xpath', '//span[@id="acrCustomerReviewText"]//text()'),
+                 ]
+        return rules
+
+    # 评分，星级
+    @staticmethod
+    def rules_scores():
+        rules = [('re', 'class="reviewCountTextLinkedHistogram noUnderline" title="(.*) out of 5 stars'),
+                 ('re', '>([\d. ]+)out of 5 stars'),
+                 ]
+        return rules
+
+    # 价格
+    @staticmethod
+    def rules_price():
+        rules = [('re', 'class="a-size-medium a-color-price">(.*)</span>'),
+                 ('re', 'class="a-color-price a-text-bold">(.*)</span>'),
+                 ('xpath', '//td[@class="a-color-price a-size-medium a-align-bottom"]/text()'),
+                 (
+                     'xpath',
+                     '//button[@class="av-button av-button--default js-purchase-button dv-record-reftag"]/text()'),
+                 ]
+        return rules
+
+    # 品牌
+    @staticmethod
+    def rules_pinpai():
+        rules = [('re', 'id="bylineInfo".*>+(.*)</a>'),
+                 ('re', 'id="gc-brand-name-link".*>+(.*)</a>'),
+                 ]
+        return rules
+
+    # 上架时间
+    @staticmethod
+    def rules_sell_time():
+        rules = [('re', '<li><b> Date first available at Amazon.co.uk:</b> (.*)</li>'),
+                 ('xpath', '//tr[@class="date-first-available"]/td[@class="value"]/text()'),
+                 ('re', '<td class="label">Date First Available</td>'),
+                 ]
+        return rules
+
+    # 图片url
+    @staticmethod
+    def rules_pic():
+        rules = [('xpath', '//div[@class="imgTagWrapper"]/img/@src'),
+                 ('xpath', '//div[@class="av-fallback-packshot"]/img/@src'),
+                 ('xpath', '//div[@class="imgTagWrapper"]/img/@data-old-hires'),
+                 ('xpath', '//div[@class="imgTagWrapper"]/img/@data-a-dynamic-image'),
+                 ]
+        return rules
+
+    # 自营
+    @staticmethod
+    def rules_ziying():
+        rules = [('re', 'sold by Amazon.'),
+                 ]
+        return rules
+
+class SP_by_key:
+
+    @staticmethod
+    def sp():
+        rules = [('re_S', '<div.*?="a-popover-sp-info-popover-(.*?)"><span>These are ads'),
+                 ('re', '<div.*?="a-popover-sponsored-header-(.*?)"><span>These are ads'),
+                 ('xpath_replace', "//div[contains(@id,'a-popover-sp-info-popover-')]/@id"),
+                 ]
+        return rules
+
+class Search_by_key:
+
+    @staticmethod
+    def asin_by_key():
+        rules = [('xpath', '//div[contains(@data-cel-widget,"search_result_")]/@data-asin'),
+                 ('xpath', '//div[@class="s-result-list sg-row"]//div/@data-asin'),
+                 ('xpath', '//li[contains(@id,"result_")]//@data-asin'),
+                 ]
+        return rules
